1cb6608e0bbd956379468d33e0dbbb188bde246d
Ethan Kieser <ethankieser@gmail.com>
2020-07-02 13:23:06 -0500

    updated for deployment

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies-data.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -18,6 +19,16 @@ app.use(function validateBearerToken(req, res, next) {
       return res.status(401).json({ error: 'Unauthorized request' })
     }
     next()
+})
+
+app.use((error) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
 })
 
 app.get('/movie', (req, res) => {
@@ -44,7 +55,7 @@ app.get('/movie', (req, res) => {
     res.json(response)
 })
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
